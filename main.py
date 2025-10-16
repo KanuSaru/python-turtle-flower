41d77d324cd5b1351be833aa941a4248c7317ac9
Kanu <erickfabiandonado@gmail.com>
2025-10-15 20:08:58 -0500

    Added random colors and documentation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import turtle # This is what I did this for :p
+import turtle # This uses the turtle graphics library to draw shapes
 import time # For the sleep function
 import random
 
@@ -7,7 +7,7 @@ t = turtle.Turtle()
 t.speed(0)
 t.pensize(2)
 turtle.bgcolor("black")
-color = (255, 255, 255)
+color = (random.random(), random.random(), random.random())
 
 # Function
 def zero_out_turtle():
@@ -16,19 +16,19 @@ def zero_out_turtle():
     t.pendown()
     t.setheading(0)
     
+# Draw a flower with a given number of repetitions and angle
 def flower(repetitions, angle):
     t.pencolor(color)
     for i in range(repetitions):
         t.fd(i)
         t.left(90)
-        t.fd(i / 2)
+        t.fd(i / 2) 
         t.right(angle)
         
 # The magic
 for i in range(3, 20):
-    flower(200, i * 10)
+    flower(300, i * 1) # Change the multiplier to change the angle, so you get different shapes!
     time.sleep(1)
     zero_out_turtle()
 
 turtle.done()
-turtle.exitonclick()
